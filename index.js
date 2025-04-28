c0faadf00e5d28e84911090e968f2e296b2318ba
Mcllivia <whois.mari.work@gmail.com>
2025-04-28 17:22:41 -0300

    rota

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const express = require ("express");
-const app = express ();
-const cors = require ("cors");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
+
+const usuariosRoute = require("./routes/login.route")
 
 app.use(cors());
 app.use(helmet());
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use((req,res,next) => {
-    res.header("Access-Control-llow-Origin", "*");
-    res.header("Acess-Control-Allow-Headers", "Origin, X-Requested-With,Content-TYpe,Accept, Authorization");
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Acess-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type,Accept, Authorization");
     if (req.method === 'OPTIONS') {
-        res.header ("Access-Control-Allow-Methods","PUT, POST, GET, PATCH, DELETE");
+        res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
     }
-    
-    
-    next ();
-})
+
+
+    next();
+});
+
+app.use('/usuarios', usuariosRoute);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require("http");
 const app = require("./index");
 
 const server = http.createServer(app);
-server.listen(300,() => {
+server.listen(3000,() => {
     console.log("Api iniciada na porta 3000");
 });
